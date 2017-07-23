37c3a6e7767e9caacc4e15a78a0ef433bdcf74af
Thanh Nam <jimmy180983@gmail.com>
2017-07-23 23:10:45 +0700

    Add files via upload

---
+++ src/reducers/updownpoint.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+import ChangeColor from '../actions/change_color';
+class Controller extends Component {
+  onDownKey(){
+    this.props.dispatch({type: 'DOWN'})
+  }
+  render(){
+    return(
+      <div>
+        <button onClick={ () => {
+            this.props.dispatch({type: 'UP'})
+        }}>Up</button>
+        <button onClick={this.onDownKey.bind(this)}>Down</button>
+        <ChangeColor />
+      </div>
+    )
+  }
+}
+
+export default connect()(Controller)
